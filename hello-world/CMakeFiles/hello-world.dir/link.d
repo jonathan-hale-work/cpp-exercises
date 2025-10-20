9e8ba1ed230abbe343bacc138b57a6cfff0662af
Jonathan Hale <jonathan.hale.work@gmail.com>
2025-10-20 08:25:44 -0600

    Hello World Exercise

---
+++ hello-world/CMakeFiles/hello-world.dir/link.d
@@ -0,0 +1,37 @@
+hello-world: \
+ /data/data/com.termux/files/usr/lib/crtbegin_dynamic.o \
+ CMakeFiles/hello-world.dir/hello_world_test.cpp.o \
+ CMakeFiles/hello-world.dir/hello_world.cpp.o \
+ CMakeFiles/hello-world.dir/test/tests-main.cpp.o \
+ /system/lib64/liblog.so \
+ /data/data/com.termux/files/usr/lib/libc++_shared.so \
+ /data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a \
+ /data/data/com.termux/files/usr/lib/libunwind.a \
+ /system/lib64/libdl.so \
+ /system/lib64/libm.so \
+ /system/lib64/libc.so \
+ /data/data/com.termux/files/usr/lib/crtend_android.o
+
+/data/data/com.termux/files/usr/lib/crtbegin_dynamic.o:
+
+CMakeFiles/hello-world.dir/hello_world_test.cpp.o:
+
+CMakeFiles/hello-world.dir/hello_world.cpp.o:
+
+CMakeFiles/hello-world.dir/test/tests-main.cpp.o:
+
+/system/lib64/liblog.so:
+
+/data/data/com.termux/files/usr/lib/libc++_shared.so:
+
+/data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a:
+
+/data/data/com.termux/files/usr/lib/libunwind.a:
+
+/system/lib64/libdl.so:
+
+/system/lib64/libm.so:
+
+/system/lib64/libc.so:
+
+/data/data/com.termux/files/usr/lib/crtend_android.o:
---
+++ readme.md
@@ -0,0 +1,9 @@
+# CPP Exercises
+
+This project is for working on C++ skills by completing the Exercism.org C++ learning track. The target platform is a Termux environment running on a Samsung Android smartphone.
+
+## Project Structure
+
+## Running an Exercise
+
+## Porting Exercises to thr Android Termux Environment
