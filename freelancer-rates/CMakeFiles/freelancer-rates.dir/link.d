4a1dc4fee3438a7ef53b1d575e8a3dcf12b04925
Jonathan Hale <jonathan.hale.work@gmail.com>
2025-11-28 07:32:59 -0700

    daily rates calculate daily rate

---
+++ freelancer-rates/CMakeFiles/freelancer-rates.dir/link.d
@@ -0,0 +1,34 @@
+freelancer-rates: \
+ /data/data/com.termux/files/usr/lib/crtbegin_dynamic.o \
+ CMakeFiles/freelancer-rates.dir/freelancer_rates_test.cpp.o \
+ CMakeFiles/freelancer-rates.dir/test/tests-main.cpp.o \
+ /system/lib64/liblog.so \
+ /data/data/com.termux/files/usr/lib/libc++_shared.so \
+ /data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a \
+ /data/data/com.termux/files/usr/lib/libunwind.a \
+ /system/lib64/libdl.so \
+ /system/lib64/libm.so \
+ /system/lib64/libc.so \
+ /data/data/com.termux/files/usr/lib/crtend_android.o
+
+/data/data/com.termux/files/usr/lib/crtbegin_dynamic.o:
+
+CMakeFiles/freelancer-rates.dir/freelancer_rates_test.cpp.o:
+
+CMakeFiles/freelancer-rates.dir/test/tests-main.cpp.o:
+
+/system/lib64/liblog.so:
+
+/data/data/com.termux/files/usr/lib/libc++_shared.so:
+
+/data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a:
+
+/data/data/com.termux/files/usr/lib/libunwind.a:
+
+/system/lib64/libdl.so:
+
+/system/lib64/libm.so:
+
+/system/lib64/libc.so:
+
+/data/data/com.termux/files/usr/lib/crtend_android.o:
