a032e4fefbe6860486eed07ecfaf677f71336927
Jonathan Hale <jonathan.hale.work@gmail.com>
2026-03-01 08:44:10 -0700

    swap first and last char toward middle

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -4,6 +4,15 @@ namespace reverse_string {
 
 // TODO: add your solution here
 	std::string reverse_string(std::string s){
+		
+		std::string r{""};
+		int len = s.length();
+		for (int i = 0; i < len/2; i++) {
+			int far = len - i -1;
+			char farChar  = s.at(far);
+			s.at(far) = s.at(i);
+			s.at(i) =farChar;
+		}
 		return s;
 	}
 
