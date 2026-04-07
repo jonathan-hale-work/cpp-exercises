bb8882dc380d7b3f2775fc5bf9918723d241d68e
Jonathan Hale <jonathan.hale.work@gmail.com>
2026-04-07 12:41:16 -0600

    Interest and making the grade

---
+++ interest-is-interesting/CMakeFiles/interest-is-interesting.dir/link.d
@@ -0,0 +1,34 @@
+interest-is-interesting: \
+ /data/data/com.termux/files/usr/lib/crtbegin_dynamic.o \
+ CMakeFiles/interest-is-interesting.dir/interest_is_interesting_test.cpp.o \
+ CMakeFiles/interest-is-interesting.dir/test/tests-main.cpp.o \
+ /system/lib64/liblog.so \
+ /data/data/com.termux/files/usr/lib/libc++_shared.so \
+ /data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a \
+ /data/data/com.termux/files/usr/lib/libunwind.a \
+ /system/lib64/libdl.so \
+ /system/lib64/libm.so \
+ /system/lib64/libc.so \
+ /data/data/com.termux/files/usr/lib/crtend_android.o
+
+/data/data/com.termux/files/usr/lib/crtbegin_dynamic.o:
+
+CMakeFiles/interest-is-interesting.dir/interest_is_interesting_test.cpp.o:
+
+CMakeFiles/interest-is-interesting.dir/test/tests-main.cpp.o:
+
+/system/lib64/liblog.so:
+
+/data/data/com.termux/files/usr/lib/libc++_shared.so:
+
+/data/data/com.termux/files/usr/lib/clang/21/lib/linux/libclang_rt.builtins-aarch64-android.a:
+
+/data/data/com.termux/files/usr/lib/libunwind.a:
+
+/system/lib64/libdl.so:
+
+/system/lib64/libm.so:
+
+/system/lib64/libc.so:
+
+/data/data/com.termux/files/usr/lib/crtend_android.o:
